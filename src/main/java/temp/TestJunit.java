571d7f77689d568c3cb57770b97f562aede7e826
suyog Godse <suyog.godse@staples.com>
2023-09-18 21:52:48 -0700

    first time commit

---
+++ src/main/java/temp/TestJunit.java
@@ -0,0 +1,17 @@
+package temp;
+
+import org.junit.Test;
+
+public class TestJunit {
+
+   /* public static void main(String[] args) {
+
+        System.out.println("Hello Suyog");
+    }*/
+
+    @Test
+    public void SampleTests()
+    {
+        System.out.println("Junit Tests");
+    }
+}
